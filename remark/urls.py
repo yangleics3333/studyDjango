3da9ec5400935f9a59cfe995b1584fe745f09879
yanglei <yl@educloudcn.com>
2021-02-20 21:15:42 +0800

    update

--- remark/urls.py
+++ remark/urls.py
@@ -4,7 +4,7 @@
 from django.urls import path
 from remark import views
 
-app_name = "Remark"  # 应用的命名空间
+app_name = "Remark"  # 应用的命名空间,用于反向查询
 
 urlpatterns = [
     path('insertSave/', views.handle_insert_save, name='insertSave'),
@@ -13,8 +13,13 @@ urlpatterns = [
     path('insertFor/', views.handle_insert_for, name='insertFor'),
     path('update/', views.handle_update, name='update'),
     path('updateBulk/', views.handle_update_bulk, name='updateBulk'),
-    path('select/', views.handle_select, name='select'),
-    path('selectWhere/', views.handle_select_where, name='selectWhere'),
     path('delete/', views.handle_delete, name='delete'),
     path('deleteBulk/', views.handle_delete_bulk, name='deleteBulk'),
+    path('classNews/<class_name>/', views.class_news, name='classNews'),
+    path('select/', views.handle_select, name='select'),
+    path('selectWhere/', views.handle_select_where, name='selectWhere'),
+    path('selectNested/', views.handle_select_nested, name='selectNested'),
+    path('selectCount/', views.handle_select_count, name='selectCount'),
+    path('selectGroup/', views.handle_select_group, name='selectGroup'),
+    path('selectQF/', views.handle_select_qf, name='selectQF'),
 ]
--- remark/views.py
+++ remark/views.py
@@ -3,9 +3,13 @@ from remark.models import DjangoTest
 from remark.models import AnalysisCommonNews
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.db.models import Max, Min, Sum, Avg, Count, Q, F
+
 from random import randint
 
-
+#centos7安装
+#yum install gcc mariadb-devel
+#pip3 install mysqlclient
 # pip install mysqlclient
 
 
@@ -33,6 +37,7 @@ def handle_insert_for(requests):
         items.append(DjangoTest(title=firsts[randint(0, 4)] +
                                       last[randint(0, 4)] +
                                       str(randint(111, 999))))
+    # 传入一个列表
     DjangoTest.objects.bulk_create(items)
     return HttpResponse('适用for批量插入')
 
@@ -94,6 +99,18 @@ def handle_update_bulk(requests):
     return HttpResponse('批量更新字段数据')
 
 
+def class_news(request, class_name):
+    # between
+    # items = AnalysisCommonNews.objects.filter(id__range=[2, 5])
+
+    items = AnalysisCommonNews.objects.filter(news_time__gt='2021-02-08 13:27:00').filter(
+        second_class__contains=class_name).filter(is_show=0)
+    for data in items:
+        # print(data.id, data.title, data.second_class, data.is_show, data.is_check, data.news_time)
+        print(data.id, data.similar_id, data.second_class, data.title)
+    return HttpResponse('查询新闻数据')
+
+
 def handle_select(requests):
     # 查询数据
     # all 过滤器 查询所有数据
@@ -115,3 +132,43 @@ def handle_select_where(requests):
         print(type(i), len(data))
         print(i.id, i.title, i.article_source, i.article_tags)
     return HttpResponse(data)
+
+
+def handle_select_nested(request):
+    # 嵌套查询
+    # 只能返回一个value字段不能多个
+    items = AnalysisCommonNews.objects.filter(id__gt=2, id__lt=5).values('id')
+    data = list(AnalysisCommonNews.objects.filter(id__in=items))
+    for i in data:
+        print(i.title, i.second_class)
+    return HttpResponse('嵌套查询')
+
+
+def handle_select_count(request):
+    # 要引用from django.db.models import Max, Min, Sum, Avg, Count
+    count = AnalysisCommonNews.objects.aggregate(Count('id')).get('id__count')
+    print(count, type(count))
+    return HttpResponse(count)
+
+
+def handle_select_group(request):
+    # similar_id分组内容，id是统计的个数
+    # values是分组字典，annotate是统计函数
+    # select similar_id,count('id') from analysis_common_news group by similar_id
+    res = AnalysisCommonNews.objects.values('similar_id').annotate(Count('id'))
+    print(res)
+    return HttpResponse(res)
+
+
+def handle_select_qf(request):
+    # Q对象用于多个查询，可以组合
+    # & = and
+    # | = or
+    # ~ = not
+    # 主要用于逻辑或，本身filter就是逻辑或
+    # items = AnalysisCommonNews.objects.filter(Q(id=2) | Q(id=5) | Q(news_id='d59907f2b1812330ab7876d282b8f220'))
+    # 不大于4
+    items = AnalysisCommonNews.objects.filter(~Q(id__gte=4))
+    for data in items:
+        print(data.id, data.similar_id, data.second_class, data.title)
+    return HttpResponse('ok')
